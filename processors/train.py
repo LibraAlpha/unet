482bcb26979a3397b139eb208d8c71af536cadf9
ych <yangchenhao8909@gmail.com>
2023-05-09 18:39:18 +0800

    add train method

--- processors/train.py
+++ processors/train.py
@@ -1,9 +1,14 @@
+import logging
+
 import numpy as np
 from model.unet import Unet
 from utils.carvana import CarvanaDataset
 from pathlib import Path
-from torch.utils.data import Dataset, DataLoader, RandomSampler, SubsetRandomSampler,random_split
+from torch.utils.data import Dataset, DataLoader, RandomSampler, SubsetRandomSampler, random_split
 from torchvision.transforms import transforms
+from torch import optim
+import torch.nn as nn
+
 
 def train(
         model,
@@ -11,6 +16,7 @@ def train(
         epochs=5,
         batch_size=1,
         learning_rate=1e-5,
+        weight_decay=1e-8
 ):
     root = Path('../data/')
     transform = transforms.Compose([
@@ -28,9 +34,24 @@ def train(
     train_data_loader = DataLoader(train_data_set, batch_size=batch_size, shuffle=True)
     test_data_loader = DataLoader(test_data_set, batch_size=batch_size, shuffle=False)
 
+    logging.info(f"""
+        Start Training with:
+        Epochs: {epochs}
+        Batch size: {batch_size}
+        Learning rate: {learning_rate}
+        Device: {device}        
+                 """)
 
+    optimizer = optim.RMSprop(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=5)
+    criterion = nn.CrossEntropyLoss()
+
+    # loop epochs
+    for epoch in range(epochs):
+        model.train()
 
     return
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
+    print('END')
